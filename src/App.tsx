391c1bd04f94368fcb5ae13dde975500475a724d
Chan <vicg0923@gmail.com>
2026-01-08 11:44:59 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     <>
       <div className="font-black">main</div>
       <div className="text-blue-400 bg-amber-50">테스트테스트</div>
+      <div className="text-blue-400 bg-green-50">테스트테스트</div>
     </>
   );
 }
