509e9ccfb3e6c1906cc4605109c95b461b7458e9
Chan <vicg0923@gmail.com>
2026-01-08 11:43:22 +0900

    프로젝트 폴더 생성

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="font-black">main</div>
+      <div className="text-blue-400 bg-amber-50">테스트테스트</div>
+    </>
+  );
+}
+
+export default App;
