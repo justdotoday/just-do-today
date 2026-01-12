d46654a8f80ef471c6811d1f92eb7d083c764766
Chan <vicg0923@gmail.com>
2026-01-12 11:08:57 +0900

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+// App.tsx
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Signup from './component/SignUp';
 
 function App() {
   return (
     <>
-      <div className="font-black">main</div>
-      <div className="text-blue-400 bg-amber-50">테스트테스트</div>
-      <div className="text-blue-400 bg-green-50">테스트테스트</div>
+      <Routes>
+        {/* 기본 레이아웃 */}
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="font-black">main</div>
+              <div className="text-blue-400 bg-amber-50">테스트테스트</div>
+              <Link to="/signup" className="text-blue-400 bg-green-50">
+                회원가입
+              </Link>
+            </>
+          }
+        />
+
+        {/* 회원가입 페이지 */}
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
   );
 }
