1b8ec8073c842ffccadb9e0694705d1bc43e4bf4
Chan <vicg0923@gmail.com>
2026-01-13 13:29:58 +0900

    koe 101

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,16 +1,20 @@
 const SignUp = () => {
   const KAKAO_AUTH_URL =
-    'https://kauth.kakao.com/oauth/authorize' +
-    '?client_id=' +
+    'https://kauth.kakao.com/oauth/authorize?client_id=' +
     import.meta.env.VITE_KAKAO_REST_API_KEY +
     '&redirect_uri=http://localhost:5173/auth/kakao/callback' +
     '&response_type=code';
 
-  console.log('KAKAO_AUTH_URL', KAKAO_AUTH_URL);
+  console.log(KAKAO_AUTH_URL);
 
   return (
     <div>
-      <a href={KAKAO_AUTH_URL}>카카오 아이디로 시작하기</a>
+      <div>Title</div>
+      <div>logo</div>
+      <div className="flex gap-2 flex-col items-center">
+        <a href="">구글 아이디로 시작하기</a>
+        <a href={KAKAO_AUTH_URL}> 카카오 아이디로 시작하기</a>
+      </div>
     </div>
   );
 };
