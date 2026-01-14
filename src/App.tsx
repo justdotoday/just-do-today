e1b63615ba5cfa80e426f0c3a0a76e90dd392e87
Chan <vicg0923@gmail.com>
2026-01-14 12:49:47 +0900

    feat:signup 초안 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Signup from './pages/SignUp';
-import KakaoCallback from './pages/auth/KakaoCallBack';
+import KakaoCallback from './pages/auth/KakaoCallback';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
@@ -13,18 +14,22 @@ function App() {
           path="/"
           element={
             <>
-              <div className="font-black">
-                홈화면 구성 전 회원가입 버튼 테스트용
+              <div className="font-black">추후 통합</div>
+              <div className="flex flex-col">
+                <Link to="/signup" className="text-blue-400 bg-green-50">
+                  회원가입
+                </Link>
+                <Link to="/main" className="text-blue-400 bg-green-50">
+                  메인페이지
+                </Link>
               </div>
-              <Link to="/signup" className="text-blue-400 bg-green-50">
-                회원가입
-              </Link>
             </>
           }
         />
 
-        {/* 회원가입 페이지 */}
+        {/* 라우팅 */}
         <Route path="/signup" element={<Signup />} />
+        <Route path="/main" element={<MainPage />} />
         <Route path="/auth/kakao/callback" element={<KakaoCallback />} />
       </Routes>
     </>
