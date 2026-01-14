2f0b8395fabd247fff8a55407e1b67bc70b04429
Leesuhwan1445 <lhh080@naver.com>
2026-01-14 10:28:10 +0900

    fix: remove broken submodule link and track client as normal directory

---
+++ client/vite-project/src/component/SignUp.tsx
@@ -0,0 +1,16 @@
+const SignUp = () => {
+  return (
+    <div>
+      <div>Title</div>
+      <div>logo</div>
+      <div>
+        <div className="flex gap-2 flex-col items-center">
+          <a href="">구글 아이디로 시작하기</a>
+          <a href="">카카오 아이디로 시작하기</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
