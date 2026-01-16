394e637f76e385f7807cf94ee2629f88363752b1
jiyoun2 <danalci17@gmail.com>
2026-01-16 12:44:33 +0900

    springdoc 및 swagger 적용

--- server/src/main/java/com/example/just_do_today/domain/auth/controller/MemberController.java
+++ server/src/main/java/com/example/just_do_today/domain/auth/controller/MemberController.java
@@ -1,4 +1,4 @@
-package com.example.just_do_today.domain.member.controller;
+package com.example.just_do_today.domain.auth.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
