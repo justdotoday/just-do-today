865280805c665026bb0435f1c526e4df394f43b3
Chan <vicg0923@gmail.com>
2026-01-19 11:35:52 +0900

    docs. app.tsx수정

--- client/vite-project/src/App.tsx
+++ client/vite-project/src/App.tsx
@@ -1,17 +1,15 @@
+// App.tsx
 import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Signup from './pages/SignUp';
-// import KakaoCallback from './auth/KaKaoCallback';
+// import KakaoCallback from './pages/auth/KakaoCallback';
 import MainPage from './pages/MainPage';
-import Home from './pages/Home';
-import CreateHabit from './pages/habbit/CreateHabit';
-import Layout from './layout/Layout';
 
 function App() {
   return (
-    <Routes>
-      {/* Layout 적용 구간 */}
-      <Route element={<Layout />}>
+    <>
+      <Routes>
+        {/* 기본 레이아웃 */}
         <Route
           path="/"
           element={
@@ -24,27 +22,18 @@ function App() {
                 <Link to="/main" className="text-blue-400 bg-green-50">
                   메인페이지
                 </Link>
-
-                <Link to="/home" className="text-blue-400 bg-green-50">
-                  홈
-                </Link>
-                <Link to="/createHabit" className="text-blue-400 bg-green-50">
-                  습관 생성
-                </Link>
               </div>
             </>
           }
         />
 
+        {/* 라우팅 */}
+
         <Route path="/signup" element={<Signup />} />
         <Route path="/main" element={<MainPage />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/createHabit" element={<CreateHabit />} />
-      </Route>
-
-      {/* Layout 없이 따로 빼고 싶은 라우트가 있으면 여기 */}
-      {/* <Route path="/auth/kakao/callback" element={<KakaoCallback />} /> */}
-    </Routes>
+        {/* <Route path="/auth/kakao/callback" element={<KakaoCallback />} /> */}
+      </Routes>
+    </>
   );
 }
 
