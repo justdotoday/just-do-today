70b44bed4a7830172d89bcbb31b6b0fc2c974ffa
Leesuhwan1445 <lhh080@naver.com>
2026-01-19 13:05:26 +0900

    수정

--- server/src/main/java/com/example/just_do_today/global/SecurityConfig.java
+++ server/src/main/java/com/example/just_do_today/global/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.just_do_today.global.config;
+package com.example.just_do_today.global;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ server/src/main/java/com/example/just_do_today/global/exception/ErrorCode.java
@@ -0,0 +1,5 @@
+package com.example.just_do_today.global.exception;
+
+public class ErrorCode {
+    
+}
