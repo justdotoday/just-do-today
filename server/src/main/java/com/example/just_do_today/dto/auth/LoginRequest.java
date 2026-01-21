f85cd022f4cee0ec48cfa1bef381491981deca62
jiyoun2 <danalci17@gmail.com>
2026-01-21 11:50:55 +0900

    feat: create kakao login

--- server/src/main/java/com/example/just_do_today/dto/auth/LoginRequest.java
+++ server/src/main/java/com/example/just_do_today/dto/auth/LoginRequest.java
@@ -1,4 +1,15 @@
 package com.example.just_do_today.dto.auth;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LoginRequest {
+
+    private String provider;
+    private String providerId;
+    private String profileImageUrl;
 }
--- server/src/main/java/com/example/just_do_today/mapper/member/MemberMapper.java
+++ server/src/main/java/com/example/just_do_today/mapper/member/MemberMapper.java
@@ -1,4 +1,25 @@
 package com.example.just_do_today.mapper.member;
 
-public class MemberMapper {
+import com.example.just_do_today.dto.member.MemberRequest;
+import com.example.just_do_today.dto.member.MemberResponse;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface MemberMapper {
+
+    @Select("SELECT * FROM member WHERE provider = #{provider} AND provider_id = #{providerId}")
+    MemberResponse findByProviderId(@Param("provider") String provider,@Param("providerId") String providerId);
+
+    @Insert("""
+        INSERT INTO member (
+            nickname, profile_image_url, user_code, 
+            provider, provider_id, user_role, 
+            created_at, updated_at
+        ) VALUES (
+            #{nickname}, #{profileImageUrl}, #{userCode}, 
+            #{provider}, #{providerId}, #{userRole}, 
+            NOW(), NOW()
+        )
+    """)
+    void saveMember(MemberRequest memberRequest);
 }
