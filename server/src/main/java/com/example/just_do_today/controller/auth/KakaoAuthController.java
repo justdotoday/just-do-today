9a8ba3df8845280f124cac84d07e648a7860770c
jiyoun2 <danalci17@gmail.com>
2026-01-21 11:50:55 +0900

    feat: create kakao login

---
+++ server/src/main/java/com/example/just_do_today/controller/auth/KakaoAuthController.java
@@ -0,0 +1,22 @@
+package com.example.just_do_today.controller.auth;
+
+import com.example.just_do_today.dto.member.MemberResponse;
+import com.example.just_do_today.service.auth.KakaoAuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/oauth/kakao")
+public class KakaoAuthController {
+
+    private final KakaoAuthService kakaoAuthService;
+
+    @GetMapping("/callback")
+    public MemberResponse callback(@RequestParam String code) {
+        return kakaoAuthService.loginWithCode(code);
+    }
+}
