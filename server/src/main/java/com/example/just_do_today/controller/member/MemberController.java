399bf1291ab6eff568551579785048f3ff6a524f
jiyoun2 <danalci17@gmail.com>
2026-01-21 13:14:13 +0900

    fix: swagger 누락 추가

--- server/src/main/java/com/example/just_do_today/controller/member/MemberController.java
+++ server/src/main/java/com/example/just_do_today/controller/member/MemberController.java
@@ -1,6 +1,7 @@
 package com.example.just_do_today.controller.member;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -9,7 +10,8 @@ import java.util.Map;
 @RestController
 public class MemberController {
 
-    @GetMapping("/test")
+
+    @GetMapping("/")
     public String getTest() {
         return "TEST Controller 연결";
     }
