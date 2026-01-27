69121495c40496af525a0eea3d032100278ec0e4
Chan <vicg0923@gmail.com>
2026-01-27 11:17:28 +0900

    fix.conflict resolve

--- client/vite-project/src/App.tsx
+++ client/vite-project/src/App.tsx
@@ -3,10 +3,9 @@ import './App.css';
 import Signup from './pages/SignUp';
 // import KakaoCallback from './auth/KaKaoCallback';
 import MainPage from './pages/MainPage';
-import Home from './pages/Home';
+import Home from './pages/habit/Home';
 import CreateHabit from './pages/habit/CreateHabit';
 import Layout from './layout/Layout';
-<<<<<<< HEAD
 import { Toaster } from 'react-hot-toast';
 
 function App() {
@@ -15,7 +14,7 @@ function App() {
       <Toaster position="top-center" />
 
       <Routes>
-        {/* Layout 적용 구간 */}
+        햐{/* Layout 적용 구간 */}
         <Route element={<Layout />}>
           <Route
             path="/"
@@ -29,7 +28,6 @@ function App() {
                   <Link to="/main" className="text-blue-400 bg-green-50">
                     메인페이지
                   </Link>
-
                   <Link to="/home" className="text-blue-400 bg-green-50">
                     홈
                   </Link>
@@ -40,52 +38,15 @@ function App() {
               </>
             }
           />
-=======
-
-function App() {
-  return (
-    <Routes>
-      {/* Layout 적용 구간 */}
-      <Route element={<Layout />}>
-        <Route
-          path="/"
-          element={
-            <>
-              <div className="font-black">추후 통합</div>
-              <div className="flex flex-col">
-                <Link to="/signup" className="text-blue-400 bg-green-50">
-                  회원가입
-                </Link>
-                <Link to="/main" className="text-blue-400 bg-green-50">
-                  메인페이지
-                </Link>
-
-                <Link to="/home" className="text-blue-400 bg-green-50">
-                  홈
-                </Link>
-                <Link to="/createHabit" className="text-blue-400 bg-green-50">
-                  습관 생성
-                </Link>
-              </div>
-            </>
-          }
-        />
-
-        <Route path="/signup" element={<Signup />} />
-
-        <Route path="/home" element={<Home />} />
-        <Route path="/createHabit" element={<CreateHabit />} />
-      </Route>
->>>>>>> feat.onboardingFixed
-
-      {/* Layout 없이 따로 빼고 싶은 라우트가 있으면 여기 */}
-      {/* kakao callback 잠시 미사용처리 */}
-      {/* <Route path="/auth/kakao/callback" element={<KakaoCallback />} /> */}
-<<<<<<< HEAD
-=======
-      <Route path="/main" element={<MainPage />} />
->>>>>>> feat.onboardingFixed
-    </Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/createHabit" element={<CreateHabit />} />
+        </Route>
+        {/* Layout 없이 따로 빼고 싶은 라우트 */}
+        {/* <Route path="/auth/kakao/callback" element={<KakaoCallback />} /> */}
+      </Routes>
+    </>
   );
 }
 
