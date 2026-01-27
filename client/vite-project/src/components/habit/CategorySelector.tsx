890d4b9bb7fe642cb07d4bb60c5d3198f00750f5
Leesuhwan1445 <lhh080@naver.com>
2026-01-27 12:09:16 +0900

    해결

--- client/vite-project/src/components/habit/CategorySelector.tsx
+++ client/vite-project/src/components/habit/CategorySelector.tsx
@@ -1,7 +1,5 @@
 //카테고리 선택 컴포넌트
 
-import React from 'react';
-
 export type CategoryItem = { name: string; icon?: string };
 
 type Props = {
--- client/vite-project/src/components/ui/shared/BottomCTA.tsx
+++ client/vite-project/src/components/ui/shared/BottomCTA.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const BottomCTA = () => {
   return <div>BottomCTA</div>;
 };
