c1699081966aa81c064f276cc1723a141c30604e
jiyoun2 <danalci17@gmail.com>
2026-01-29 12:10:52 +0900

    feat: habit post/get api 생성

--- server/src/main/java/com/example/just_do_today/service/habit/HabitService.java
+++ server/src/main/java/com/example/just_do_today/service/habit/HabitService.java
@@ -1,4 +1,54 @@
 package com.example.just_do_today.service.habit;
 
+import com.example.just_do_today.domain.Habit.Habit;
+import com.example.just_do_today.domain.Habit.UserHabit;
+import com.example.just_do_today.domain.Habit.UserHabitSchedule;
+import com.example.just_do_today.dto.habit.CreateHabitRequestDto;
+import com.example.just_do_today.dto.habit.HabitResponseDto;
+import com.example.just_do_today.mapper.habit.HabitMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class HabitService {
+
+    private final HabitMapper habitMapper;
+
+    @Transactional // 에러 시 모든 트랜잭션 취소
+    public void createHabit(Long memberId, CreateHabitRequestDto dto) {
+        // Habit 저장
+        Habit habit = new Habit();
+        habit.setName(dto.getName());
+        habit.setIsPublic(dto.getIsPublic());
+        habitMapper.saveHabit(habit);
+
+        // UserHabit 저장
+        UserHabit userHabit = new UserHabit();
+        userHabit.setMemberId(memberId);
+        userHabit.setHabitId(habit.getId());
+        userHabit.setStatus("ACTIVE");
+        userHabit.setFrequency(dto.getFrequency());
+        userHabit.setStartDate(dto.getStartDate());
+        habitMapper.saveUserHabit(userHabit);
+
+        if (dto.getDays() != null) {
+            for (Integer day : dto.getDays()) {
+                UserHabitSchedule schedule = new UserHabitSchedule();
+                schedule.setUserHabitId(userHabit.getId());
+                schedule.setDayOfWeek(day);
+                schedule.setTimesPerWeek(dto.getDays().size());
+                habitMapper.saveSchedule(schedule);
+            }
+        }
+
+    }
+    // 습관 조회 (읽어오기만 하므로)
+    @Transactional(readOnly = true)
+    public List<HabitResponseDto> getHabitList(Long memberId) {
+        return habitMapper.findAllByMemberId(memberId);
+    }
 }
